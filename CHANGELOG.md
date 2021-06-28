396e4d8ceb378f473886383022b7e7763914c53c
Leander Philippo <lphilippo@adventive.es>
2021-06-28 18:49:40 +0200

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+## grindnu/magento2-digital-ocean-spaces :: CHANGELOG
+
+`grindnu/magento2-digital-ocean-spaces` is a module that allows to connect with Digital Ocean Spaces for Remote Storage.
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+
+## [Unreleased]
+
+**Added**
+
+**Changed**
+
+**Fixed**
+
+**Removed**
+
+
+## [1.0.0] - 2021-06-28
+
+Initial release prior to versioning.
---
+++ Driver/DigitalOceanS3Factory.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace Grindnu\DigitalOceanSpaces\Driver;
+
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Cached\CachedAdapter;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\RemoteStorage\Driver\Cache\CacheFactory;
+use Magento\RemoteStorage\Driver\DriverException;
+use Magento\RemoteStorage\Driver\DriverFactoryInterface;
+use Magento\RemoteStorage\Driver\RemoteDriverInterface;
+use Magento\RemoteStorage\Model\Config;
+
+/**
+ * Creates a pre-configured instance of DigitalOcean S3 driver.
+ */
+class DigitalOceanS3Factory implements DriverFactoryInterface
+{
+    private const DIGITAL_OCEAN_SPACES_ENDPOINT = 'https://%s.digitaloceanspaces.com/';
+
+    /**
+     * @var ObjectManagerInterface
+     */
+    private $objectManager;
+
+    /**
+     * @var CacheFactory
+     */
+    private $cacheFactory;
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @param ObjectManagerInterface $objectManager
+     * @param CacheFactory $cacheFactory
+     * @param Config $config
+     */
+    public function __construct(ObjectManagerInterface $objectManager, CacheFactory $cacheFactory, Config $config)
+    {
+        $this->objectManager = $objectManager;
+        $this->cacheFactory = $cacheFactory;
+        $this->config = $config;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function create(): RemoteDriverInterface
+    {
+        try {
+            return $this->createConfigured(
+                $this->config->getConfig(),
+                $this->config->getPrefix(),
+                $this->config->getCacheAdapter(),
+                $this->config->getCacheConfig()
+            );
+        } catch (LocalizedException $exception) {
+            throw new DriverException(__($exception->getMessage()), $exception);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createConfigured(
+        array $config,
+        string $prefix,
+        string $cacheAdapter,
+        array $cacheConfig
+    ): RemoteDriverInterface {
+        $config['version'] = 'latest';
+
+        if (empty($config['credentials']['key']) || empty($config['credentials']['secret'])) {
+            unset($config['credentials']);
+        }
+
+        if (empty($config['bucket']) || empty($config['region'])) {
+            throw new DriverException(__('Bucket and region are required values'));
+        }
+
+        if (!empty($config['http_handler'])) {
+            $config['http_handler'] = $this->objectManager->create($config['http_handler'])($config);
+        }
+
+        // Override endpoint to allow DigitalOcean.
+        $config['endpoint'] = sprintf(self::DIGITAL_OCEAN_SPACES_ENDPOINT, $config['region']);
+
+        $client = new S3Client($config);
+        $adapter = new AwsS3Adapter($client, $config['bucket'], $prefix);
+
+        return $this->objectManager->create(
+            AwsS3::class,
+            [
+                'adapter' => $this->objectManager->create(CachedAdapter::class, [
+                    'adapter' => $adapter,
+                    'cache' => $this->cacheFactory->create($cacheAdapter, $cacheConfig)
+                ]),
+                'objectUrl' => $client->getObjectUrl($adapter->getBucket(), $adapter->applyPathPrefix('.'))
+            ]
+        );
+    }
+}
