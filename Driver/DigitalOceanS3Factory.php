5d0475a48900fd1f31fcb5cc21d9dc0dae5f1bca
Leander Philippo <lphilippo@adventive.es>
2021-10-13 11:49:59 +0200

    Cleanup

--- Driver/DigitalOceanS3Factory.php
+++ Driver/DigitalOceanS3Factory.php
@@ -4,15 +4,53 @@ namespace Grindnu\DigitalOceanSpaces\Driver;
 
 use Magento\AwsS3\Driver\AwsS3Factory;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\ObjectManagerInterface;
+use Magento\RemoteStorage\Driver\Adapter\Cache\CacheInterfaceFactory;
+use Magento\RemoteStorage\Driver\Adapter\CachedAdapterInterfaceFactory;
+use Magento\RemoteStorage\Driver\Adapter\MetadataProviderInterfaceFactory;
 use Magento\RemoteStorage\Driver\DriverException;
 use Magento\RemoteStorage\Driver\RemoteDriverInterface;
+use Magento\RemoteStorage\Model\Config;
 
 /**
- * Creates a pre-configured instance of DigitalOcean S3 driver.
+ * Creates a pre-configured instance of AWS S3 driver.
  */
 class DigitalOceanS3Factory extends AwsS3Factory
 {
     private const DIGITAL_OCEAN_SPACES_ENDPOINT = 'https://%s.digitaloceanspaces.com/';
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @param ObjectManagerInterface $objectManager
+     * @param Config $config
+     * @param MetadataProviderInterfaceFactory $metadataProviderFactory
+     * @param CacheInterfaceFactory $cacheInterfaceFactory
+     * @param CachedAdapterInterfaceFactory $cachedAdapterInterfaceFactory
+     * @param string|null $cachePrefix
+     */
+    public function __construct(
+        ObjectManagerInterface $objectManager,
+        Config $config,
+        MetadataProviderInterfaceFactory $metadataProviderFactory,
+        CacheInterfaceFactory $cacheInterfaceFactory,
+        CachedAdapterInterfaceFactory $cachedAdapterInterfaceFactory,
+        string $cachePrefix = null
+    ) {
+        $this->config = $config;
+
+        parent::__construct(
+            $objectManager,
+            $config,
+            $metadataProviderFactory,
+            $cacheInterfaceFactory,
+            $cachedAdapterInterfaceFactory,
+            $cachePrefix
+        );
+    }
 
     /**
      * @inheritDoc
@@ -26,9 +64,7 @@ class DigitalOceanS3Factory extends AwsS3Factory
 
             return $this->createConfigured(
                 $config,
-                $this->config->getPrefix(),
-                $this->config->getCacheAdapter(),
-                $this->config->getCacheConfig()
+                $this->config->getPrefix()
             );
         } catch (LocalizedException $exception) {
             throw new DriverException(__($exception->getMessage()), $exception);
